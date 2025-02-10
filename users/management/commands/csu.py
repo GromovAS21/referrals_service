85d0adf2b833f4838c6570ff2818989d81cba2b1
Anton Gromov <seed14112@yandex.ru>
2025-02-10 15:32:30 +0300

    [+] Добавление команды для создания superuser`а

---
+++ users/management/commands/csu.py
@@ -0,0 +1,21 @@
+чfrom django.core.management import BaseCommand
+from users.models import User
+
+class Command(BaseCommand):
+    """Создание суперпользователя"""
+
+    def handle(self, *args, **options):
+
+        try:
+            user = User.objects.create(
+                email="admin@test.ru",
+                is_staff=True,
+                is_superuser=True,
+            )
+            user.set_password("Qwerty")
+            user.save()
+        except Exception:
+            self.stdout.write(self.style.ERROR("SUPERUSER CREATE FAILED"))
+        else:
+            self.stdout.write(self.style.SUCCESS("SUPERUSER CREATE SUCCESS"))
+
