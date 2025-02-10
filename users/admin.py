be6a9864302240d6c6dc6cd5275462b7c8d15255
Anton Gromov <seed14112@yandex.ru>
2025-02-10 14:07:36 +0300

    Добавление модели User и админка

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Админка для модели User"""
+
+    list_display = ("email", "is_active", "is_staff", "is_superuser")
+    list_filter = ("is_active", "is_staff", "is_superuser")
+    search_fields = ("email",)
