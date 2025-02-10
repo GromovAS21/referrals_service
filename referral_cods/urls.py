331610fda7cc4bd84bee34e0ab397516ea930a0e
Anton Gromov <seed14112@yandex.ru>
2025-02-10 23:02:53 +0300

    [+] Добавление ручки для удаления кода

--- referral_cods/urls.py
+++ referral_cods/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from referral_cods.apps import ReferralCodesConfig
-from referral_cods.views import CreateReferralCodeView
+from referral_cods.views import CreateReferralCodeView, DeleteReferralCodeView
 
 app_name = ReferralCodesConfig.name
 
 urlpatterns = [
     path("", CreateReferralCodeView.as_view(), name="create_referral_code"),
+    path("<int:pk>/", DeleteReferralCodeView.as_view(), name="delete_referral_code"),
 ]
