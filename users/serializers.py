48d4e0468ceb1f2c70b2c1f2dc7c2256f477df91
Anton Gromov <seed14112@yandex.ru>
2025-02-10 18:25:58 +0300

    [~] Убрана переменная password
    R: Не было необходимости в этой строке, так как она переопредела в модели
    
    FB: Удалена строка

--- users/serializers.py
+++ users/serializers.py
@@ -6,8 +6,6 @@ from users.models import User
 class CreateSerializer(serializers.ModelSerializer):
     """Сериализатор для создания пользователя"""
 
-    password = serializers.CharField(required=True)
-
     class Meta:
         model = User
         fields = ("email", "password")
