e606dde12b955f92187d6d45c891189bd7dd85c9
Anton Gromov <seed14112@yandex.ru>
2025-02-10 22:56:08 +0300

    [+] Создание команды cu

---
+++ users/management/commands/cu.py
@@ -0,0 +1,30 @@
+import os
+
+import django
+from django.core.management import BaseCommand
+from users.models import User
+
+class Command(BaseCommand):
+    """Создание пользователя"""
+
+    def handle(self, *args, **options):
+        users = [
+            {"email": "user_1@test.ru", "password": "Qwerty1"},
+            {"email": "user_2@test.ru", "password": "Qwerty2"},
+        ]
+
+        users_for_create = []
+
+        for user in users:
+            users_for_create.append(User(**user))
+            users_for_create[-1].set_password(user["password"])
+
+        try:
+            User.objects.bulk_create(users_for_create)
+        except django.db.utils.IntegrityError:
+            self.stdout.write(self.style.ERROR("USERS ALREADY CREATED"))
+        except Exception:
+            self.stdout.write(self.style.ERROR("USERS CREATE FAILED"))
+        else:
+            self.stdout.write(self.style.SUCCESS("USERS CREATE SUCCESS"))
+
