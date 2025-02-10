539c40b3eab7a046076ae4e94d141f7d5c20aed9
Anton Gromov <seed14112@yandex.ru>
2025-02-10 18:20:28 +0300

    [~] Изменение  validate_date_in_past
    R: Не работал код
    
    FB: написана корректная дата datetime.now().date()

--- referrals/validators.py
+++ referrals/validators.py
@@ -5,5 +5,6 @@ from rest_framework.exceptions import ValidationError
 
 def validate_date_in_past(value):
     """Проверка на дату в прошлом"""
-    if value < datetime.date().today():
+
+    if value < datetime.now().date():
         raise ValidationError("Дата не может быть в прошлом")
