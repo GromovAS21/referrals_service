d4fc0706b0f80612ec0b22d09a58b2c9305a85c5
Anton Gromov <seed14112@yandex.ru>
2025-02-10 18:05:26 +0300

    [+] Добавление валидаторов для модели User

--- users/models.py
+++ users/models.py
@@ -1,17 +1,31 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 from django.db import models
 
+
 class User(AbstractUser):
+    """Модель пользователя"""
 
     username = None
 
-    email = models.EmailField(unique=True)
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Email",
+    )
+    password = models.CharField(
+        max_length=128,
+        verbose_name="Пароль",
+        validators=[
+            MinValueValidator(8, "Пароль должен содержать не менее 8 символов")
+        ]
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email
+
 
     class Meta:
         verbose_name = 'Пользователь'
