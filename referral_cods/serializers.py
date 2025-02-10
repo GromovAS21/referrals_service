c4287e076cd48371237de8d5bfbc59f5c101e3a4
Anton Gromov <seed14112@yandex.ru>
2025-02-10 18:42:24 +0300

    [+] Добавление сериализатора  CreateReferralCodeSerializer

---
+++ referral_cods/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from referral_cods.models import Referral
+
+
+class CreateReferralCodeSerializer(serializers.ModelSerializer):
+    """Сериализатор для создания реферального кода"""
+
+    class Meta:
+        model = Referral
+        fields = ("code", "validity_period",)
